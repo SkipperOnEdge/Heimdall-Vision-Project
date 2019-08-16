33b338b5c4d2ce33797853d8ba487ee6532096bc
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2019-08-16 12:27:34 -0500

    Drivetrain

---
+++ include/Constants.h
@@ -0,0 +1,7 @@
+#ifndef Constants_h
+#define Constants_h
+
+#define LEFT_PORT 11
+#define RIGHT_PORT 13
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <Constants.h>
 
 Servo left;
+Servo right;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  left.attach(LEFT_PORT);
+  right.attach(RIGHT_PORT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+    int leftAmount = Serial.parseInt();
+    int rightAmount = Serial.parseInt();
+
+    if (Serial.read() == '\n') {
+      left.write(leftAmount);
+      right.write(rightAmount);
+    }
+  }
 }
